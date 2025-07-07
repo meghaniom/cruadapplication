97949febddeef8c14f794193604a0192b419f3d6
om meghani <meghanio264@gmail.com>
2025-07-07 18:04:10 +0530

    code change

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt =  require('jsonwebtoken');
+const JWT_SECRET =  "ommeghani";
+
+const authMiddleWare = (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.staus(404).json({message : "Unauthorized: No token provided"});
+    }
+    const token =  authHeader.split(" ")[1];
+
+     try {
+        const decode = jwt.verify(token, JWT_SECRET);
+        req.user = decode;
+        next();
+     }
+     catch(error) {
+        return res.status(401).json({message : "Unauthorized: Invalid or expired token"})
+     }
+};
+
+ module.exports =  authMiddleWare;
--- routes/todorouter.js
+++ routes/todorouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const TodoContoller = require("../controllers/Todo");
+const authMiddleWare = require('../middleware/auth');
 
-router.post("/createtodo", TodoContoller.createTodo);
-router.patch("/:id", TodoContoller.updateTodo);
-router.get("/",TodoContoller.readTodo);
-router.get("/:id", TodoContoller.singleTodo);
-router.delete("/:id",TodoContoller.deleteTodo);
+router.post("/createtodo",authMiddleWare, TodoContoller.createTodo);
+router.patch("/:id",authMiddleWare, TodoContoller.updateTodo);
+router.get("/", authMiddleWare,TodoContoller.readTodo);
+router.get("/:id",authMiddleWare, TodoContoller.singleTodo);
+router.delete("/:id",authMiddleWare,TodoContoller.deleteTodo);
 
 module.exports = router;
