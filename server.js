50f269886ac211f72fb6bf7faebd8b085b637bb6
om meghani <meghanio264@gmail.com>
2025-07-07 18:52:30 +0530

    cros error

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const dbConnect = require("./config/dbConnect");
 const app = express();
 const userRouter = require("./routes/userrouter");
 const TodoRouter = require("./routes/todorouter");
+const cors = require('cros');
 
 dbConnect().then(() => {
   console.log("Database connected successfully");
 });
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true 
+}));
+
 app.use(express.json());
 
 const PORT = 3000;
