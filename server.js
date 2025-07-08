e4730ec3ec88e3b22a22fae7b0fa39f854a80ad1
om meghani <meghanio264@gmail.com>
2025-07-08 10:17:29 +0530

    local changre

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const dbConnect = require("./config/dbConnect");
 const app = express();
 const userRouter = require("./routes/userrouter");
 const TodoRouter = require("./routes/todorouter");
-const cors = require('cros');
-
+const cors = require('cors');
 dbConnect().then(() => {
   console.log("Database connected successfully");
 });
